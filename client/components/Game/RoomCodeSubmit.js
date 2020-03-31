6419c313fe3792b55109a884dcdbea55fbc069c3
rosebutch <annathedm@gmail.com>
2020-03-31 11:22:46 -0500

    added to use effect array in room code submit

--- client/components/Game/RoomCodeSubmit.js
+++ client/components/Game/RoomCodeSubmit.js
@@ -12,15 +12,18 @@ const RoomCodeSubmit = props => {
   ); // finds game session with same code
   const [noCode, setNoCode] = useState(false);
 
-  useEffect(() => {
-    if (session) {
-      const newPlayerRef = db.ref(
-        "gameSessions/" + formCode + "/players/" + uid
-      ); // only if the session already exists, i can make a reference to the new player
-      newPlayerRef.set({ nickname: nick, host: false, points: 0 }); // setting the new player using the ref
-      history.push(`/games/${formCode}`); // redirecting to the game page
-    } else setNoCode(true);
-  }, []);
+  useEffect(
+    () => {
+      if (session) {
+        const newPlayerRef = db.ref(
+          "gameSessions/" + formCode + "/players/" + uid
+        ); // only if the session already exists, i can make a reference to the new player
+        newPlayerRef.set({ nickname: nick, host: false, points: 0 }); // setting the new player using the ref
+        history.push(`/games/${formCode}`); // redirecting to the game page
+      } else setNoCode(true);
+    },
+    [history, session, uid]
+  );
 
   if (noCode) return <Alert variant="danger">This code does not exist</Alert>;
   if (loading) return "";
