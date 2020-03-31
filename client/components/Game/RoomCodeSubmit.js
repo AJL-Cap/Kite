563cb435265a3fe0e310ac2d3f0654a17156d2fa
Ji Hye Park <jhp0621@gmail.com>
2020-03-31 11:17:15 -0500

    fixed creating nonexisting room code in db

--- client/components/Game/RoomCodeSubmit.js
+++ client/components/Game/RoomCodeSubmit.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import fire from "../../fire";
 import { useObject } from "react-firebase-hooks/database";
+import Alert from "react-bootstrap/Alert";
 
 const db = fire.database();
 
@@ -9,22 +10,21 @@ const RoomCodeSubmit = props => {
   const [session, loading, error] = useObject(
     db.ref("gameSessions/" + formCode)
   ); // finds game session with same code
-  useEffect(
-    () => {
-      if (session) {
-        const newPlayerRef = db.ref(
-          "gameSessions/" + formCode + "/players/" + uid
-        ); // only if the session already exists, i can make a reference to the new player
-        newPlayerRef.set({ nickname: nick, host: false, points: 0 }); // setting the new player using the ref
-        history.push(`/games/${formCode}`); // redirecting to the game page
-      }
-    },
-    [history, session, uid]
-  );
+  const [noCode, setNoCode] = useState(false);
 
+  useEffect(() => {
+    if (session) {
+      const newPlayerRef = db.ref(
+        "gameSessions/" + formCode + "/players/" + uid
+      ); // only if the session already exists, i can make a reference to the new player
+      newPlayerRef.set({ nickname: nick, host: false, points: 0 }); // setting the new player using the ref
+      history.push(`/games/${formCode}`); // redirecting to the game page
+    } else setNoCode(true);
+  }, []);
+
+  if (noCode) return <Alert variant="danger">This code does not exist</Alert>;
   if (loading) return "";
   if (error) return <div className="alert-warning">incorrect room code</div>;
-  return <></>;
 };
 
 export default RoomCodeSubmit;
