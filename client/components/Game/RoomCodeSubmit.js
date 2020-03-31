98ac03cc8b3c8dc1073ccac3452c56f838ab67e8
rosebutch <annathedm@gmail.com>
2020-03-31 12:19:05 -0500

    cryign

--- client/components/Game/RoomCodeSubmit.js
+++ client/components/Game/RoomCodeSubmit.js
@@ -9,7 +9,6 @@ const RoomCodeSubmit = props => {
   const [session, loading, error] = useObject(
     db.ref("gameSessions/" + formCode)
   ); // finds game session with same code
-  //const [noCode, setNoCode] = useState(false);
 
   useEffect(
     () => {
@@ -19,7 +18,7 @@ const RoomCodeSubmit = props => {
         ); // only if the session already exists, i can make a reference to the new player
         newPlayerRef.set({ nickname: nick, host: false, points: 0 }); // setting the new player using the ref
         history.push(`/games/${formCode}`); // redirecting to the game page
-      } //else setNoCode(true);
+      }
     },
     [history, session, uid]
   );
