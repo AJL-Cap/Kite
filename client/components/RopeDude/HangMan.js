612885dab99c548a4243e6f8cf8fa24f4d4e3c64
Ji Hye Park <jhp0621@gmail.com>
2020-04-02 17:48:56 -0500

    hangman pics + webpack loader

--- client/components/RopeDude/HangMan.js
+++ client/components/RopeDude/HangMan.js
@@ -1,13 +1,12 @@
 import React from "react";
-import fire from "../../fire";
-import ASCIIART from "./ASCIIART";
+import images from "../../images/images";
 
 const HangMan = props => {
   const { code, session } = props;
   const points = session.points;
 
   //rendering hangman based on current points
-  return <div> {ASCIIART[points / 20]} </div>;
+  return <img src={images[points / 20]} />;
 };
 
 export default HangMan;
